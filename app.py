93d8e1b0920bab6b19f32da223a555849d5e53b7
Gilbert Icheboi <gilbert.icheboi@student.moringaschool>
2025-06-20 02:26:32 -0700

    solution

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config["MAIL_USE_SSL"] = False
-app.config['MAIL_USERNAME'] = 'gilbert.icheboi@student.moringaschool.com'
+app.config['MAIL_USERNAME'] = 'gilbert.icheboi@student.com'
 app.config['MAIL_PASSWORD'] = 'rzcy ydmk sutz yzmq' 
 app.config['MAIL_DEFAULT_SENDER'] = 'yourmail@gmail.com'
 
